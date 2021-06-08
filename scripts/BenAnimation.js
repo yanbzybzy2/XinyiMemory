fd2d518d065314dc11027d89803c6801f30fa6f5
ben yan <yanbzybzy2@gmail.com>
2021-06-08 09:36:12 +0100

    initial site

---
+++ scripts/BenAnimation.js
@@ -0,0 +1,28 @@
+﻿import { gsap } from "./gsap/index.js";
+import { ScrollTrigger } from "./gsap/ScrollTrigger.js";
+
+console.log("Hello World JS");
+
+gsap.registerPlugin(ScrollTrigger);
+
+/*
+window.BenAnimation = () => {
+    gsap.to("#img1", {
+        scrollTrigger: {
+            trigger: "#img1",
+            start: "top top", // when the top of the trigger hits the top of the viewport
+            end: "bottom bottom", // end after scrolling 500px beyond the start
+            scrub: true,
+            markers: true
+        },
+        duration: 10,
+        x: 100
+    });
+
+
+
+}
+*/
+
+// Adding gsap for dev console debug
+window.gsap = gsap;
